687a4b5a51902d4a553cac2fbab3b6c795b6b7d3
danieltistoj <josetisrey@gmail.com>
2023-06-06 17:38:35 -0600

    ejercicios con int y double en java

---
+++ TiposVariables/src/tiposvariables/TiposVariables.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposvariables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TiposVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        int edad = 28;
+        System.out.println("Edad: "+edad);
+        edad = 47;
+        System.out.println("Edad: "+edad);
+        /*Double*/
+        double salario = 1250.60;
+        System.out.println("Salario: "+salario);
+        double salarioMitad = salario / 2;
+        System.out.println("Salario Mita: "+salarioMitad);
+        
+    }
+    
+}
