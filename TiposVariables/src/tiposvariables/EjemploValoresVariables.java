b0a898b91a47eb376ec7df42a232f946e38ad5f0
danieltistoj <josetisrey@gmail.com>
2023-06-06 21:03:00 -0600

    variables y memoria

---
+++ TiposVariables/src/tiposvariables/EjemploValoresVariables.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposvariables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjemploValoresVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 9;
+        System.out.println(numero2);
+        numero2 = numero1;
+        System.out.println(numero2);
+        numero1 = 88;
+        System.out.println(numero2);
+    }
+    
+}
