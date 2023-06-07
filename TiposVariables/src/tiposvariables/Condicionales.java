aa195d5383f4d46864708cb005cf25eb5cdae64d
danieltistoj <josetisrey@gmail.com>
2023-06-06 21:36:05 -0600

    condicion if

---
+++ TiposVariables/src/tiposvariables/Condicionales.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposvariables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Condicionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad = 17;
+        if (edad >= 18) {
+            System.out.println("Usted puede entrar");
+        }else{
+            System.out.println("Usted no puede entrar");
+        }
+    }
+    
+}
