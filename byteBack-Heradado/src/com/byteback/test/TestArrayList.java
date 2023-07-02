38ab2cf76addfafb1b3c5693afb2759849f7f1e6
danieltistoj <josetisrey@gmail.com>
2023-07-01 18:13:04 -0600

    for con arraylist

--- byteBack-Heradado/src/com/byteback/test/TestArrayList.java
+++ byteBack-Heradado/src/com/byteback/test/TestArrayList.java
@@ -18,13 +18,16 @@ public class TestArrayList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList array = new ArrayList<>();
+        ArrayList<Cuenta> array = new ArrayList<>();
         Cuenta cc1 = new CuentaCorriente(11, 22);
         Cuenta cc2 = new CuentaCorriente(12,42);
         array.add(cc1);
         array.add(cc2);
         
-        System.out.println((CuentaCorriente) array.get(0));
+        //System.out.println((CuentaCorriente) array.get(0));
+        for(Cuenta cuenta:array){
+            System.out.println(cuenta);
+        }
         
         
     }
